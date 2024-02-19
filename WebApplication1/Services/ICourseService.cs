0a7ac6ad832130ef043919893bd481608904028b
Ismoil Tohirov <ismoiltohirov55@gmail.com>
2024-02-19 21:21:49 +0500

    added new functions and interface services

--- WebApplication1/Services/ICourseService.cs
+++ WebApplication1/Services/ICourseService.cs
@@ -1,6 +1,12 @@
-﻿namespace WebApplication1.Services
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Services
 {
     public interface ICourseService
     {
+        public void DeleteCourse();
+        public string CreateCourse(Course course);
+        public void UpdateCourse(Course course);
+
     }
 }
--- WebApplication1/Services/IStudentService.cs
+++ WebApplication1/Services/IStudentService.cs
@@ -1,6 +1,11 @@
-﻿namespace WebApplication1.Services
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Services
 {
     public interface IStudentService
     {
+        public Student GetStudent(int id);
+        public void UpdateStudent(Student student);
+        public void DeleteStudent(int id);
     }
 }
